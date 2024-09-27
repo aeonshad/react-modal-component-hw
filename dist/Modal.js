41051bff6643195817bb0ead0df983960699c420
aeonshad <shadaeon@gmail.com>
2024-09-28 00:17:24 +0200

    Compiling the component

--- dist/Modal.js
+++ dist/Modal.js
@@ -5,23 +5,32 @@ import React from 'react';
 export const Modal = ({
   className,
   open,
-  children
-}) => open ? /*#__PURE__*/React.createElement("div", {
-  className: clsx('react-modal-component-hw', className)
-}, children) : null;
+  children,
+  ariaLabelledBy,
+  ariaDescribedBy
+}) => open && /*#__PURE__*/React.createElement("div", {
+  className: clsx('react-modal-component-hw', className),
+  role: "dialog",
+  "aria-modal": "true",
+  "aria-labelledby": ariaLabelledBy,
+  "aria-describedby": ariaDescribedBy,
+  tabIndex: "-1"
+}, children);
 export const ModalContent = ({
   className,
   onClose,
   size = 25,
   stroke = '#000000',
-  children
+  children,
+  ariaLabel
 }) => /*#__PURE__*/React.createElement("div", {
   className: clsx('react-modal-component-hw-container', className)
 }, children, /*#__PURE__*/React.createElement(X, {
   className: "react-modal-component-hw-icon",
   size: size,
   stroke: stroke,
-  onClick: onClose
+  onClick: onClose,
+  "aria-label": ariaLabel
 }));
 export const ModalHeader = ({
   className,
@@ -31,15 +40,19 @@ export const ModalHeader = ({
 }, children);
 export const ModalTitle = ({
   className,
-  children
+  children,
+  id
 }) => /*#__PURE__*/React.createElement("h2", {
-  className: clsx('react-modal-component-hw-title', className)
+  className: clsx('react-modal-component-hw-title', className),
+  id: id
 }, children);
 export const ModalDescription = ({
   className,
-  children
+  children,
+  id
 }) => /*#__PURE__*/React.createElement("p", {
-  className: clsx('react-modal-component-hw-description', className)
+  className: clsx('react-modal-component-hw-description', className),
+  id: id
 }, children);
 export const ModalFooter = ({
   className,
