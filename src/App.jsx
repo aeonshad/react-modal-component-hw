faaba131a80a7be8ea75ffb329420b54036fb9a8
aeonshad <shadaeon@gmail.com>
2024-09-27 18:46:34 +0200

    Add clsx to modify style with own class

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,56 @@ import './styles/reset.css';
 import './styles/main.css';
 
 function App() {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpenDefaultModal, setIsOpenDefaultModal] = useState(false);
+    const [isOpenCustomModal, setIsOpenCustomModal] = useState(false);
 
-    const toggleModal = () => {
-        setIsOpen(!isOpen);
+    const toggleModalDefaultModal = () => {
+        setIsOpenDefaultModal(!isOpenDefaultModal);
+    };
+    const toggleModalCustomModal = () => {
+        setIsOpenCustomModal(!isOpenCustomModal);
     };
     return (
         <main className="main-container">
-            <button className="btn" onClick={toggleModal}>
-                Open Modal
+            <button className="btn" onClick={toggleModalCustomModal}>
+                Open modal
             </button>
-            <Modal open={isOpen}>
-                <ModalContent onClose={toggleModal}>
+            <Modal open={isOpenDefaultModal}>
+                <ModalContent onClose={toggleModalDefaultModal}>
                     <ModalHeader>
-                        <ModalTitle>Test Title</ModalTitle>
-                        <ModalDescription>This is a test description.</ModalDescription>
+                        <ModalTitle>Default modal</ModalTitle>
+                        <ModalDescription>This is a default modal without custom style.</ModalDescription>
                     </ModalHeader>
-                    <ModalFooter>Test Footer</ModalFooter>
+                    <ModalFooter>
+                        <button
+                            onClick={() => {
+                                toggleModalDefaultModal();
+                                toggleModalCustomModal();
+                            }}
+                        >
+                            Custom modal
+                        </button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+            <Modal className="custom-modal" open={isOpenCustomModal}>
+                <ModalContent className="custom-modal-container" onClose={toggleModalCustomModal} stroke="#dfdfdf" size={32}>
+                    <ModalHeader className="custom-modal-header">
+                        <img className="custom-modal-img" src="./hero.svg" alt="hero" />
+                        <ModalTitle className="custom-modal-title">Welcome to React Modal Component HW</ModalTitle>
+                        <ModalDescription className="custom-modal-description">You are ready to use my component!</ModalDescription>
+                    </ModalHeader>
+                    <ModalFooter className="custom-modal-footer">
+                        <button
+                            className="custom-modal-button"
+                            onClick={() => {
+                                toggleModalDefaultModal();
+                                toggleModalCustomModal();
+                            }}
+                        >
+                            Default modal
+                        </button>
+                    </ModalFooter>
                 </ModalContent>
             </Modal>
         </main>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,20 @@
 import { X } from 'lucide-react';
 import './modal.css';
+import clsx from 'clsx';
 
-export const Modal = ({ className, open, children }) => (open ? <div className="modal-overlay">{children}</div> : null);
+export const Modal = ({ className, open, children }) => (open ? <div className={clsx('react-modal-component-hw', className)}>{children}</div> : null);
 
 export const ModalContent = ({ className, onClose, size = 25, stroke = '#000000', children }) => (
-    <div className="modal-container">
+    <div className={clsx('react-modal-component-hw-container', className)}>
         {children}
-        <X className="modal-icon" size={size} stroke={stroke} onClick={onClose} />
+        <X className="react-modal-component-hw-icon" size={size} stroke={stroke} onClick={onClose} />
     </div>
 );
 
-export const ModalHeader = ({ className, children }) => <div className="modal-header">{children}</div>;
+export const ModalHeader = ({ className, children }) => <div className={clsx('react-modal-component-hw-header', className)}>{children}</div>;
 
-export const ModalTitle = ({ className, children }) => <h2 className="modal-title">{children}</h2>;
+export const ModalTitle = ({ className, children }) => <h2 className={clsx('react-modal-component-hw-title', className)}>{children}</h2>;
 
-export const ModalDescription = ({ className, children }) => <p className="modal-description">{children}</p>;
+export const ModalDescription = ({ className, children }) => <p className={clsx('react-modal-component-hw-description', className)}>{children}</p>;
 
-export const ModalFooter = ({ className, children }) => <div className="modal-footer">{children}</div>;
+export const ModalFooter = ({ className, children }) => <div className={clsx('react-modal-component-hw-footer', className)}>{children}</div>;
